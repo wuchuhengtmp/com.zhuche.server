4acf750dce074e5340591474ed4bc40bf9596925
wuchuheng <root@wuchuheng.com>
2021-11-22 20:09:18 +0800

    feat: create the customer scalar that names NonNegativeInt

--- src/main/java/com/zhuche/server/resolver/bank/mutation/dto/CreateBankAccountInput.java
+++ src/main/java/com/zhuche/server/resolver/bank/mutation/dto/CreateBankAccountInput.java
@@ -13,4 +13,5 @@ import lombok.Data;
 @Data
 public class CreateBankAccountInput {
     String firstname;
+    int age;
 }
---
+++ src/main/java/com/zhuche/server/scalar/ScalarConfig.java
@@ -0,0 +1,22 @@
+/**
+ * Desc    The controllers is part of server
+ * Author  wuchuheng <root@wuchuheng.com>
+ * Blog    https://wuchuheng.com
+ * DATE    2021/11/22
+ * Listen  MIT
+ */
+
+package com.zhuche.server.scalar;
+
+import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ScalarConfig {
+    @Bean
+    public GraphQLScalarType nonNegativeInt() {
+        return ExtendedScalars.NonNegativeInt;
+    }
+}
