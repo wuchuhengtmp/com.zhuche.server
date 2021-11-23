4ac8b80c037f085d79b29b57797c86cf1952659c
wuchuheng <root@wuchuheng.com>
2021-11-23 21:35:01 +0800

    feat: create the listener of graphql

---
+++ src/main/java/com/zhuche/server/core/LoggingListener.java
@@ -0,0 +1,49 @@
+/**
+ * Desc    The controllers is part of server
+ * Author  wuchuheng <root@wuchuheng.com>
+ * Blog    https://wuchuheng.com
+ * DATE    2021/11/23
+ * Listen  MIT
+ */
+
+package com.zhuche.server.core;
+
+import graphql.kickstart.servlet.core.GraphQLServletListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LoggingListener implements GraphQLServletListener {
+    private final Clock clock;
+
+    @Override
+    public RequestCallback onRequest(HttpServletRequest request, HttpServletResponse response) {
+        var starTime = Instant.now();
+        log.info("Received graphql request");
+        return new RequestCallback() {
+            @Override
+            public void onSuccess(HttpServletRequest request, HttpServletResponse response) {
+                // no-op
+            }
+
+            @Override
+            public void onError(HttpServletRequest request, HttpServletResponse response, Throwable throwable) {
+                // no-op
+            }
+
+            @Override
+            public void onFinally(HttpServletRequest request, HttpServletResponse response) {
+//                log.info("Completed Request. Time taken: {}", Duration.between(starTime, Instant.now(clock)));
+            }
+        };
+    }
+}
